c622cd2f98ceacee7040121e7b714f59ec226738
Nazmul0197 <sirmd97@gmail.com>
2026-09-10 22:45:23 +0600

    lesson btn added

--- script/index.js
+++ script/index.js
@@ -1,2 +1,20 @@
 //
-console.log("connected");
+const loadLesson = async () => {
+  const url = "https://openapi.programming-hero.com/api/levels/all";
+  const res = await fetch(url);
+  const data = await res.json();
+  displayLesson(data.data);
+};
+const displayLesson = (lessons) => {
+  const lessonBtnContainer = document.getElementById("lesson-btn-container");
+  lessonBtnContainer.innerHTML = "";
+  lessons.forEach((lesson) => {
+    const lessonBtn = document.createElement("div");
+    lessonBtn.innerHTML = `
+        <button onclick="" class="btn btn-outline btn-primary">Lesson-${lesson.level_no}</button>
+    `;
+    lessonBtnContainer.append(lessonBtn);
+  });
+};
+
+loadLesson();
